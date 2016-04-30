99128bd1b5f12b71119e4fbad7c25ef4e6f643a3
kero1976 <kero1976@outlook.jp>
2016-04-30 23:49:14 +0900

    終了コードのテスト用プログラム

---
+++ ExitCodeInspect/ExitCodeInspect.Tests/Class1Test.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+using System.IO;
+using 単体テスト用ユーティリティ;
+
+namespace ExitCodeInspect
+{
+    public class Class1Test
+    {
+        #region ***テスト
+
+        [TestCase("10 3 0", 10)]
+        [TestCase("11 3 1", 11)]
+        [TestCase("0 3 1", 0)]
+        [TestCase("0 0 0", 0)]
+        public void test(string args, int exitCode)
+        {
+            Process proc = new Process();
+
+            proc.StartInfo.FileName = "ExitCodeInspect.exe";
+            proc.StartInfo.Arguments = args;
+            proc.Start();
+            proc.WaitForExit();
+
+            Assert.That(proc.ExitCode, Is.EqualTo(exitCode));
+        }
+
+        #endregion
+
+        #region 前処理・後処理
+        [TestFixtureSetUp]
+        public void 全体前処理()
+        {
+        }
+
+        [TestFixtureTearDown]
+        public void 全体後処理()
+        {
+        }
+
+        [SetUp]
+        public void 個別前処理()
+        {
+        }
+
+        [TearDown]
+        public void 個別後処理()
+        {
+            // テストデータを削除
+            //FileDeleteUtil.DeleteFile();
+        }
+        #endregion
+    }
+}
---
+++ ExitCodeInspect/ExitCodeInspect/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExitCodeInspect
+{
+    /// <summary>
+    /// 指定した終了コードを指定した時間(秒)後に返す。
+    /// </summary>
+    /// 使い方：(EXE) (終了コード) (時間_秒) (0:Environment.Exit,1:Environment.ExitCode,2:return)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 引数が2つ以外の場合は使い方を表示し、終了
+            if (args.Length != 3)
+            {
+                Console.WriteLine("使い方：第一引数は終了コード。第二引数は指定時間(秒)。第三引数はタイプ。0:Exitメソッド、1:ExitCodeプロパティ");
+                return;
+            }
+            int exitCode, second, type;
+
+            if(!int.TryParse(args[0], out exitCode))
+            {
+                // 解析できない場合はデフォルトとして0をセットする。
+                exitCode = 0;
+            }
+
+            if (!int.TryParse(args[1], out second))
+            {
+                // 解析できない場合はデフォルトとして0をセットする。
+                second = 0;
+            }
+
+            if (!int.TryParse(args[2], out type))
+            {
+                // 解析できない場合はデフォルトとして0をセットする。
+                type = 0;
+            }
+
+            // 指定秒数待機する
+            Thread.Sleep(second * 1000);
+
+            switch (type)
+            {
+                case 0:
+                    Environment.Exit(exitCode);
+                    break;
+                case 1:
+                    Environment.ExitCode = exitCode;
+                    break;
+                //case 2:
+                    //return exitCode;
+            }
+            return;
+            
+
+        }
+    }
+}
