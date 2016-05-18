81c8816fe08c2792df0892a0811a987d27c76c3e
kero1976 <kero1976@outlook.jp>
2016-05-18 22:51:31 +0900

    デバッグ出力用メッセージを作成

---
+++ TestUtil/TestUtil.Tests/src/DebugUtilTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUtil.src
+{
+    class DebugUtilTest
+    {
+        [Test]
+        public void 目視確認()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                System.Threading.Thread.Sleep(100);
+                Console.WriteLine(DebugUtil.DebugString("テスト" + i));
+            }
+            //Parallel.For(0, 10, (i) =>
+            //  {
+            //      System.Threading.Thread.Sleep(100);
+            //      Console.WriteLine(DebugUtil.DebugString("テスト" + i));
+            //  });
+
+            DebugUtil.DebugPrint("コンソール出力テスト");
+        }
+    }
+}
---
+++ TestUtil/TestUtil/DebugUtil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUtil
+{
+    /// <summary>
+    /// デバッグ支援用ユーティリティクラス
+    /// </summary>
+    public static class DebugUtil
+    {
+        /// <summary>
+        /// デバッグ用文字列に成形する。
+        /// </summary>
+        /// 【HH:MI:SS.sss】【PID:プロセスID】【SID:スレッドID】デバッグ用文字列
+        /// <param name="msg">デバッグ用文字列</param>
+        /// <returns>デバッグ用文字列</returns>
+        public static string DebugString(string msg)
+        {
+            return string.Format("【{0}.{1:000}】【PID:{2}】【SID:{3:00}】{4}",
+                System.DateTime.Now.ToLongTimeString(),
+                System.DateTime.Now.Millisecond,
+                System.Diagnostics.Process.GetCurrentProcess().Id,
+                System.Threading.Thread.CurrentThread.ManagedThreadId,
+                msg);
+        }
+
+        /// <summary>
+        /// デバッグ用文字列をコンソールに出力する。
+        /// </summary>
+        /// <param name="msg">デバッグ用文字列</param>
+        public static void DebugPrint(string msg)
+        {
+            Console.WriteLine(DebugString(msg));
+        }
+    }
+}
